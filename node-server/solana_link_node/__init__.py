a3b1416b5e02f1055f7346bf9da8d0defd8fb2b0
Joseph C Wang <joequant@gmail.com>
2022-11-08 23:27:53 +0800

    add event loop

--- node-server/solana_link_node/__init__.py
+++ node-server/solana_link_node/__init__.py
@@ -5,19 +5,36 @@ import asyncio
 import os
 from dotenv import load_dotenv
 from solana.rpc.async_api import AsyncClient
+from solana.publickey import PublicKey
 
 load_dotenv()
 
 solana_node = os.getenv('SOLANA_NODE', 'http://api.devnet.solana.com')
+pk = PublicKey(
+    os.getenv('SOLANA_ADDRESS')
+)
 
 class LinkNode:
+    """Class for link node"""
     def __init__(self, poll_interval):
         self.poll_interval = poll_interval
+        self.last_signature = None
+        self.last_block_time = None
     async def process(self):
         """Process event"""
         async with AsyncClient(solana_node) as client:
-            res = await client.is_connected()
-            print(res)  # True
+            if self.last_signature is None:
+                txlist = await client.get_signatures_for_address(pk)
+            else:
+                txlist = await client.get_signatures_for_address(
+                    pk, until=self.last_signature
+                )
+            for tx in txlist.value:
+                print(tx)
+                if self.last_block_time is None or \
+                   tx.block_time > self.last_block_time:
+                    self.last_signature = tx.signature
+                    self.last_block_time = tx.block_time
     async def event_loop(self, poll_interval):
         """Run event loop"""
         while True:
